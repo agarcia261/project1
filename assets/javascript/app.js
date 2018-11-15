8523847d424af4b96360910d15568d1c1f84251b
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2018-11-14 19:20:12 -0500

    wip

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,20 @@
+var roundNumber = 1;
 $(document).ready(function () {
     $('#modal1').modal({
         dismissible: false,
         onCloseEnd: function () { timer.run() }
     });
-    $('#modal2').modal({
-        dismissible: false,
-    });
+    // $('#modal2').modal({
+    //     dismissible: false,
+    // });
     $('#modal1').modal('open');
     $("#timerNum").text(moment(timer.startNumber).format('m:ss'));
-    var roundNumber = 1;
+    
     $("#roundNumber").text(roundNumber);
 });
 
 var timer = {
-    startNumber: 120000,
+    startNumber: 10000,
     intervalId: '',
     run: function () {
         clearInterval(this.intervalId);
@@ -25,11 +26,29 @@ var timer = {
         $("#timerNum").text(formattedTime);
         if (timer.startNumber === 0) {
             timer.stop();
-            //do something here after timer hits zero
-            $('#modal2').modal('open');
+            if(roundNumber<5){
+                //do something here after timer hits zero
+                modalNextRound();
+            }
         }
     },
     stop: function () {
         clearInterval(this.intervalId);
     }
+};
+
+function modalNextRound(){
+    roundNumber++;
+    $("#roundNumber").text(roundNumber);
+    timer.startNumber = 10000;
+    $("#timerNum").text(moment(timer.startNumber).format('m:ss'));
+    var roundCompleted = $('<p>');
+    var instructions = $('<p>');
+    roundCompleted.text('Round Completed');
+    instructions.text('Click Next Round when you are ready to begin the next round.');
+    $('.modal-content').empty();
+    $('.modal-content').append(roundCompleted);
+    $('.modal-content').append(instructions);
+    $('#modal-btn').text('Next Round');
+    $('#modal1').modal('open');
 };
